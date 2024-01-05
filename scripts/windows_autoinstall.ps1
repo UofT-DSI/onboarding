09194dad21b239ef4c4e901e57345405d13038b7
Simeon Wong <dtxe@users.noreply.github.com>
2024-01-04 21:09:58 -0500

    minimum winget version

--- scripts/windows_autoinstall.ps1
+++ scripts/windows_autoinstall.ps1
@@ -5,6 +5,12 @@ if (!$winget) {
     Write-Error "winget is required to install the course software. Please see troubleshooting steps for updating your apps in the Microsoft Store."
 } 
 
+# make sure winget version > 1.6
+$winget_version = winget --version
+if ($winget_version -lt 1.6) {
+    Write-Error "winget version 1.6 or greater is required to install the course software. Please see troubleshooting steps for updating your apps in the Microsoft Store."
+}
+
 # check for git
 $git = Get-Command git -ErrorAction SilentlyContinue
 if ($git) {
